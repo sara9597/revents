6bb36565e117fb44d5653fc3601910878f9abe0a
SJ <sarajukic83@gmail.com>
2021-09-07 20:29:34 +0200

    End of section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Grid, Button } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
+import cuid from "cuid";
 
 const eventsFromDashboard = [
   {
@@ -65,6 +66,15 @@ class EventDashboard extends Component {
       isOpen:!isOpen
     }))
   }
+
+  handleCreateEvent = (newEvent) =>{
+    newEvent.id=cuid();
+    newEvent.hostPhotoURL="/assets/user.png";
+    this.setState(({events}) =>({ //razbijemo  eventove iz stanja
+      events:[...events,newEvent], //prosiri niz s novom vrijednoscu
+      isOpen:false
+    }))
+  }
   render() {
     const { events, isOpen } = this.state;
     return (
@@ -78,7 +88,7 @@ class EventDashboard extends Component {
             content='CreateEvent'
             onClick={this.handleIsOpenToggle}
           />
-          {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle} />}
+          {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle} />}
         </Grid.Column>
       </Grid>
     );
