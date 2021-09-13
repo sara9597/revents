26817cae859046835ee5f0f41cbf73dc6bb03bcd
SJ <sarajukic83@gmail.com>
2021-09-13 14:20:34 +0200

    Routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,35 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
-import NavBar from "../../nav/NavBar/NavBar";
+import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventsDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashBoard/PeopleDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
+import UserDetailedPage from "../../features/event/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Route exact path='/' component={HomePage} />
+        <Route
+          path='/(.+)'
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className='main'>
+                <Route path='/events' component={EventDashboard} />
+                <Route path='/events/:id' component={EventsDetailedPage} />
+                <Route path='/people' component={PeopleDashboard} />
+                <Route path='/profile/:id' component={UserDetailedPage} />
+                <Route path='/settings' component={SettingsDashboard} />
+                <Route path='/createEvent' component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 };
 
 if (module.hot) {
@@ -15,13 +21,8 @@ if (module.hot) {
     setTimeout(render);
   });
 }
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
