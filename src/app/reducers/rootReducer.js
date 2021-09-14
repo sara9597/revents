a1c86eff57554f34b795bc09225a4b52d525df14
SJ <sarajukic83@gmail.com>
2021-09-14 19:58:34 +0200

    Redux forms

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import testReducer from "../../features/testarea/testReducer"
 import eventReducer from "../../features/event/eventReducer";
+import {reducer as FormReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
     test:testReducer,
-    events: eventReducer
+    events: eventReducer,
+    form:FormReducer
 })
 
 export default rootReducer;
